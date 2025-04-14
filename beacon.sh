206933b5d4835c5bc6e7fb2210778bfcb42f1948
sager <xueqian1991@gmail.com>
2025-04-14 10:26:30 +0800

    add case and config

---
+++ beacon.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "beacon node with MAXPeers=${MAXPEERS} allpeer=${ALLPEERS} and EXECUTE=$EXECUTE, p2pkey=${P2PKEY}"
+
+if [ "$BOOT_DELAY" != "" ] &&  [ "$BOOT_DELAY" != "0" ];then
+                sleep $BOOT_DELAY
+fi
+
+sleep 5 && /usr/bin/beacon-chain \
+        --datadir=/root/beacondata \
+        --min-sync-peers=0 \
+        --genesis-state=/root/config/genesis.ssz \
+        --bootstrap-node "${BOOTNODE}"\
+        --interop-eth1data-votes \
+        --chain-config-file=/root/config/config.yml \
+        --p2p-max-peers=${MAXPEERS} \
+        --p2p-priv-hex=${P2PKEY} \
+        --contract-deployment-block=0 \
+        --chain-id=${CHAIN_ID:-32382} \
+        --rpc-host=0.0.0.0 \
+        --grpc-gateway-host=0.0.0.0 \
+        --execution-endpoint=http://${EXECUTE}:8551 \
+        --accept-terms-of-use \
+        --p2p-static-id=true \
+        --jwt-secret=/root/config/jwtsecret \
+        --suggested-fee-recipient=0x123463a4b065722e99115d6c222f267d9cabb524 \
+        --minimum-peers-per-subnet=0 \
+        --enable-debug-rpc-endpoints \
+        ${ALLPEERS} >> /root/beacondata/d.log 2>&1
---
+++ execute.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+initialize() {
+        if [ ! -d "/root/gethdata/geth" ]; then
+                echo "genesis init"
+                base64 -d /root/config/base64.json > /tmp/secret.json
+                /usr/bin/geth --datadir /root/gethdata init /root/config/genesis.json
+                /usr/bin/geth --datadir /root/gethdata account import /tmp/secret.json <<EOM
+123
+123
+EOM
+                echo "123" > /root/config/password.txt
+        fi
+}
+
+initialize 
+
+/usr/bin/geth --datadir /root/gethdata \
+        --http \
+        --http.api=eth,net,web3,admin,txpool,debug \
+        --http.addr=0.0.0.0 \
+        --http.corsdomain=* \
+        --ws \
+        --ws.api=eth,net,web3 \
+        --ws.addr=0.0.0.0 \
+        --ws.origins=* \
+        --authrpc.vhosts=* \
+        --authrpc.addr=0.0.0.0 \
+        --authrpc.port=8551 \
+        --authrpc.jwtsecret=/root/config/jwtsecret \
+        --datadir=gethdata \
+        --bootnodes "enode://34c9a172990c85f80df96fe601743fbed3b3a00032b96dc44d56196f04a389f680b9b087d2214265fb4a9d6d1283199f33660965a307798f51e59342bbba881f@172.99.1.200:0?discport=30301" \
+        --allow-insecure-unlock \
+        --unlock=0x123463a4b065722e99115d6c222f267d9cabb524 \
+        --password=/root/config/password.txt \
+        --syncmode=full >> /root/gethdata/d.log 2>&1
