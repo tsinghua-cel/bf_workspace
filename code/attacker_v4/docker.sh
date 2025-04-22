d3f18f82e2e3553e78beb35cd583f311d0cd62b2
sager <xueqian1991@gmail.com>
2025-04-22 14:28:44 +0800

    update script to build docker images

---
+++ code/attacker_v4/docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build --no-cache -t tscel/bunnyfinder:capella .
--- code/modified_prysm_5_1_2/docker.sh
+++ code/modified_prysm_5_1_2/docker.sh
@@ -2,5 +2,3 @@
 docker build --no-cache -t tscel/bf.prysm:v5.1.2 -f Dockerfile.prysm .
 docker build --no-cache -t tscel/prysmctl:v5.1.2 -f Dockerfile.prysmctl .
 
-#docker push tscel/bf.prysm:v5.1.2
-#docker push tscel/prysmctl:v5.1.2
