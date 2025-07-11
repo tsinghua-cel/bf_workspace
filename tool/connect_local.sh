a35458dc690cdf1180777b1c0156e6ecf6b39a89
luxq <xueqian1991@gmail.com>
2025-07-11 07:59:25 +0000

    add connect scripts

---
+++ tool/connect_local.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source .env.local
+set -e
+
+# Verify environment variables (must be set by user)
+: "${MYSQL_HOST:?MYSQL_HOST must be set (remote MySQL server IP)}"
+: "${MYSQL_PORT:=3306}"
+: "${MYSQL_USER:?MYSQL_USER must be set (MySQL username)}"
+: "${MYSQL_DATABASE:?MYSQL_DATABASE must be set (target database name)}"
+: "${MYSQL_PASSWORD:?MYSQL_PASSWORD must be set (target database password)}"
+
+
+
+# Execute commands using temporary MySQL container
+echo -e "\nConnecting ...\n"
+docker run --rm -it \
+  mysql:8.0 \
+  mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" 
+
