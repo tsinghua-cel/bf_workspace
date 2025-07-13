e160f8f248f51bdeeb1eea2f856e19801c1e319c
sager <xueqian1991@gmail.com>
2025-07-13 13:22:30 +0800

    update script

--- attack.sh
+++ attack.sh
@@ -12,28 +12,28 @@ case $casetype in
                 ./v5/runtest.sh $casetype 360
                 ;;
         "exante")
-                ./v5/runtest.sh $casetype 1800
+                ./v5/runtest.sh $casetype 3600
                 ;;
         "sandwich")
-                ./v5/runtest.sh $casetype 1800
+                ./v5/runtest.sh $casetype 3600
                 ;;
         "staircase")
-                ./v4/runtest.sh $casetype 1800
+                ./v4/runtest.sh $casetype 3600
                 ;;
         "unrealized")
-                ./v4/runtest.sh $casetype 1800
+                ./v4/runtest.sh $casetype 3600
                 ;;
         "withholding")
-                ./v4/runtest.sh $casetype 1800
+                ./v4/runtest.sh $casetype 3600
                 ;;
         "selfish")
-                ./v4/runtest.sh selfish 1800
+                ./v4/runtest.sh selfish 3600
                 ;;
         "staircase-ii")
-                ./v5/runtest.sh staircaseii 1800
+                ./v5/runtest.sh staircaseii 3600
                 ;;
         "pyrrhic-victory")
-                ./v5/runtest.sh sync 1800
+                ./v5/runtest.sh sync 3600
                 ;;
         *)
                 echo "unsupported casetype $casetype, supported cases are: $allcase"
