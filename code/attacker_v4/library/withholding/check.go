86699c4c735db7d4b9f17bf1400518050577f22b
sager <xueqian1991@gmail.com>
2025-07-13 12:13:04 +0800

    update withholding

--- code/attacker_v4/library/withholding/check.go
+++ code/attacker_v4/library/withholding/check.go
@@ -1,6 +1,7 @@
 package withholding
 
 import (
+	"github.com/tsinghua-cel/attacker-service/common"
 	"github.com/tsinghua-cel/attacker-service/types"
 	"strconv"
 )
@@ -15,8 +16,12 @@ func CheckDuties(param types.LibraryParams, duties []types.ProposerDuty) ([]inte
 		if param.IsHackValidator(valIdx) {
 			tmpsub = append(tmpsub, duty)
 		} else {
-			if len(tmpsub) > 5 {
-				result = append(result, tmpsub)
+			if len(tmpsub) >= 5 {
+				// if the latest slot is epochEnd, add it to response.
+				latest, _ := strconv.Atoi(tmpsub[len(tmpsub)-1].Slot)
+				if int64(latest) == (common.EpochEnd(common.SlotToEpoch(int64(latest)))) {
+					result = append(result, tmpsub)
+				}
 			}
 			tmpsub = make([]types.ProposerDuty, 0)
 		}
