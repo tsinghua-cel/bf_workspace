4c66d20113d4e04579001d65104d71253930116f
sager <xueqian1991@gmail.com>
2025-08-07 09:44:38 +0800

    udpate code

--- code/attacker_v4/library/withholding/block.go
+++ code/attacker_v4/library/withholding/block.go
@@ -12,8 +12,8 @@ func calcTargetTime(slot int, targetSlot int64, offset int) int64 {
 }
 
 func BlockStrategy(cur, start, end int, actions map[string]string) {
-	// delay block to next epoch + 8 slots.
-	targetSlot := common.EpochEnd(common.SlotToEpoch(int64(cur))) + 8
+	// delay block to next epoch + 3 slots.
+	targetSlot := common.EpochEnd(common.SlotToEpoch(int64(cur))) + 3
 	targetTime := calcTargetTime(cur, targetSlot, cur-start)
 	actions["BlockBeforeBroadCast"] = fmt.Sprintf("delayToMilliTime:%d", targetTime)
 }
